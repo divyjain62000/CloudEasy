b2a30d76e842544557cf34ce94d4f2c60120f564
divyjain62000 <divyjain62000@gmail.com>
2022-02-26 14:00:10 +0530

    Cloudeasy

---
+++ cloudeasy/src/test/java/com/cloudeasy/CloudeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
